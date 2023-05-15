bd0ba816ee1c276d151e216f1ca3aaaff80b5a22
cpachulski <pachulski.colin@gmail.com>
2023-05-15 13:15:33 -0600

    change the note component into smaller component-modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import CreateArea from "./components/CreateAreaObject";
 import Note from "./components/Note";
+import Modal from "./components/Modal";
 
 import "./App.css";
 
 function App() {
   const [notes, setNotes] = useState([]);
   const [isModalHiden, setIsModalHiden] = useState(false);
+  const [selectedNoteId, setSelectedNoteId] = useState("");
 
   function addNote(newNote) {
     setNotes((prevNotes) => [...prevNotes, newNote]);
@@ -17,25 +19,9 @@ function App() {
     setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
   }
 
-  function handleUpdateNote(updateNote) {
-    const { title, content, id } = updateNote;
-    const updatedNoteList = notes.map((note) => {
-      if (note.id === id) {
-        return {
-          ...note,
-          title: title,
-          content: content,
-        };
-      }
-      return note;
-    });
-    setNotes(updatedNoteList);
-    handleUpdateNoteRemovel(id, updateNote);
-  }
-
-  function handleUpdateNoteRemovel(id, updateNote) {
-    setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
-    setNotes((prevNotes) => [updateNote, ...prevNotes]);
+  function handleUpdateNote(id) {
+    setIsModalHiden((current) => !current);
+    setSelectedNoteId(id);
   }
 
   const noteList = notes.map((note) => (
@@ -46,14 +32,27 @@ function App() {
       content={note.content}
       onDelete={deleteNote}
       handleUpdateNote={handleUpdateNote}
+      isHidden={isModalHiden} //<--css fix
     />
   ));
+
+  const getData = (data) => {
+    //console.log("data", data.id);
+    setNotes((prevNotes) => prevNotes.filter((note) => note.id !== data.id));
+    setNotes((prevNotes) => [data, ...prevNotes]);
+  };
 
   return (
     <div className="App">
       <Header />
       <CreateArea onAdd={addNote} />
-      {/* <Modal isHiden={isModalHiden} SelectedNote={SelectedNote}/> */}
+      <Modal
+        isHidden={isModalHiden}
+        notes={notes}
+        selectedNoteId={selectedNoteId}
+        onSubmit={getData}
+        handleUpdateNote={handleUpdateNote}
+      />
       {noteList}
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,65 +1,15 @@
 import React, { useState } from "react";
 
-function Note({ id, title, content, onDelete, handleUpdateNote }) {
+function Note({ id, title, content, onDelete, handleUpdateNote, isHidden }) {
   const handleDelete = () => onDelete(id);
-
-  const [isEditing, setIsEditing] = useState(false);
-  const [updateNote, setUpdateNote] = useState({ id, title, content });
-  const [activeNoteId, setActiveNoteId] = useState(""); //change div
-
-  function toggleEditMode() {
-    // setIsEditing((prevCheck) => !prevCheck);
-    setIsEditing(true);
-    setActiveNoteId(id);
-  }
-
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setUpdateNote((prevNotes) => ({
-      ...prevNotes,
-
-      [name]: value,
-    }));
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    handleUpdateNote(updateNote);
-    setIsEditing(false);
-  }
-
+  // isHidden ? "hiddenNotes" : "note"  hides all css notes beside the edit
   return (
-    <div onClick={toggleEditMode}>
-      {isEditing ? (
-        <div className={isEditing && id === activeNoteId ? "hidden" : "note"}>
-          <form onSubmit={handleSubmit} className={"form_pop"}>
-            <input
-              type="text"
-              name="title"
-              value={updateNote.title}
-              onChange={handleChange}
-            />
-            <textarea
-              name="content"
-              value={updateNote.content}
-              onChange={handleChange}
-            ></textarea>
-            <button className="butn-Note">Save</button>
-
-            <button onClick={toggleEditMode} className="butn-Note">
-              {toggleEditMode && "cancel"}
-            </button>
-          </form>
-        </div>
-      ) : (
-        <div className={"note"}>
-          <h1>{title}</h1>
-          <p>{content}</p>
-          <button onClick={handleDelete}>Delete</button>
-          <br />
-          <button onClick={toggleEditMode}>Edit</button>
-        </div>
-      )}
+    <div className={"note"}>
+      <h1>{title}</h1>
+      <p>{content}</p>
+      <button onClick={handleDelete}>Delete</button>
+      <br />
+      <button onClick={() => handleUpdateNote(id)}>Edit</button>
     </div>
   );
 }
